607a552a87c4fc49c414f6e4981d4837d9640ad2
David McClure <davidwilliammcclure@gmail.com>
2015-12-14 15:41:53 -0800

    Stub in a custom grunt 'ingest' task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,15 @@
 
 
+require('babel-register');
+
+
 module.exports = function(grunt) {
 
   require('time-grunt')(grunt);
-  require('jit-grunt')(grunt);
+
+  require('jit-grunt')(grunt)({
+    customTasksDir: 'tasks'
+  });
 
   require('load-grunt-config')(grunt, {
     loadGruntTasks: false,
