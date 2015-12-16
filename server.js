57b91b0358c87601e1558feef5bcbd83727611c0
David McClure <davidwilliammcclure@gmail.com>
2015-12-16 11:55:53 -0800

    Stubbing in testing logic in the ingest task.

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 
-// TODO|dev
-db.insert({ notes: ['A', 'B', 'C'] }, function(err, doc) {
-  console.log(doc);
-});
-
-
 app.get('/api/tree', function(req, res) {
   res.send('tree');
 });
--- tasks/ingest.js
+++ tasks/ingest.js
@@ -1,7 +1,20 @@
 
 
+import _ from 'lodash';
+import db from '../database';
+
+
+console.log(db);
+
+
 module.exports = function(grunt) {
   grunt.registerTask('ingest', 'Ingest pieces.', function() {
-    console.log('ingest');
+
+    _.times(1000, function(i) {
+      db.insert({ notes: ['A', 'B', 'D'] }, function(err, doc) {
+        console.log(err);
+      });
+    });
+
   });
 }
