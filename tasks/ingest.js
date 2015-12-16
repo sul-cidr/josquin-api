6a9487dc800070632dbc72718605be741b335e86
David McClure <davidwilliammcclure@gmail.com>
2015-12-16 12:32:09 -0800

    Why won't the nedb insert work from a grunt task?

--- tasks/ingest.js
+++ tasks/ingest.js
@@ -4,14 +4,11 @@ import _ from 'lodash';
 import db from '../database';
 
 
-console.log(db);
-
-
 module.exports = function(grunt) {
   grunt.registerTask('ingest', 'Ingest pieces.', function() {
 
     _.times(1000, function(i) {
-      db.insert({ notes: ['A', 'B', 'D'] }, function(err, doc) {
+      db.insert({ test: i }, function(err, doc) {
         console.log(err);
       });
     });
